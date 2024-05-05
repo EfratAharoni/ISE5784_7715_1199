080ae1434ba1bf773b3cd7ae0da1361e57fd6708
EfratAharoni <efaharon@g.jct.ac.il>
2024-05-05 22:45:48 +0300

    Adding some class in project1

---
+++ src/geometries/Plane.java
@@ -0,0 +1,18 @@
+package geometries;
+
+import primitives.Point;
+import primitives.Vector;
+
+public class Plane implements Geometry{
+    public Plane(Point vo, Point v1, Point v2) {
+    }
+
+    public Vector getNormal() {
+        return null;
+    }
+
+    @Override
+    public Vector getNormal(Point point) {
+        return null;
+    }
+}
---
+++ src/primitives/Point.java
@@ -0,0 +1,29 @@
+package primitives;
+
+public class Point {
+    public static final Point ZERO = new Point(Double3.ZERO);
+    final Double3 xyz;
+    public Point(double x, double y, double z) {
+        xyz = new Double3(x,y,z);
+    }
+
+    public Point(Double3 xyz) {
+        this.xyz= xyz;
+    }
+
+    public Vector subtract(Point p1) {
+        return new Vector (xyz.subtract(p1.xyz));
+    }
+
+    public Point add(Vector v1) {
+        return new Point(xyz.add(v1.xyz));
+    }
+
+    public double distanceSquared(Point p1) {
+        return 14;
+    }
+
+    public double distance(Point p1) {
+        return Math.sqrt(distanceSquared(p1));
+    }
+}
