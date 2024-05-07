7df858689ad967368a397e42475c5f0b6027d8e6
EfratAharoni <efaharon@g.jct.ac.il>
2024-05-07 12:53:34 +0300

    Adding some new functions to the classes

--- src/primitives/Vector.java
+++ src/primitives/Vector.java
@@ -3,9 +3,73 @@ package primitives;
 public class  Vector extends Point {
     public Vector(double x, double y , double z ) {
         super(x,y,z);
+        if (xyz.equals(Double3.ZERO)) {
+            throw new IllegalArgumentException("Vector Zero");
+        }
     }
 
     public Vector(Double3 double3) {
         super(double3);
+        if (xyz.equals(Double3.ZERO)) {
+            throw new IllegalArgumentException("Vector Zero");
+        }
+    }
+
+    public Vector crossProduct(Vector other) {
+        double ax = xyz.d1;
+        double ay = xyz.d2;
+        double az = xyz.d3;
+
+        double bx = other.xyz.d1;
+        double by = other.xyz.d2;
+        double bz = other.xyz.d3;
+
+        double cx = ay * bz - az * by;
+        double cy = az * bx - ax * bz;
+        double cz = ax * by - ay * bx;
+
+        return new Vector(cx, cy, cz);
+    }
+
+    /**
+     * dot product between two vectors (scalar product)
+     * @param other
+     * @return new vector
+     */
+    public double dotProduct(Vector other) {
+        return other.xyz.d1 * xyz.d1 +
+                other.xyz.d2 * xyz.d2 +
+                other.xyz.d3 * xyz.d3;
+    }
+
+    /**
+     * @return the length Squared of vector
+     */
+    public double lengthSquared() {
+        return xyz.d1 * xyz.d1 +
+                xyz.d2 * xyz.d2 +
+                xyz.d3 * xyz.d3;
+    }
+
+    /**
+     * @return the sqrt of length Squared (the length of vector)
+     */
+    public double length() {
+        return Math.sqrt(lengthSquared());
+    }
+
+    /**
+     * @return Normalized vector
+     */
+    public Vector normalize() {
+        double len = length();
+        if (len == 0)
+            throw new ArithmeticException("Divide by zero!");
+        return new Vector(xyz.reduce((len)));
+    }
+
+    @Override
+    public String toString() {
+        return "Vector " + xyz;
     }
 }
